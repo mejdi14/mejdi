2a3612c56b826e21392612e55fb01b8149e3ff15
mejdi <mejdihafiane@gmail.com>
2019-12-19 10:24:36 +0100

    'update'

--- automationt.bat
+++ automationt.bat
@@ -1,4 +1,15 @@
 set -e
+echo deleting file
+
+del "E:\martin\final\mejdi\dblank.txt" /s /f /q
+
+echo Done!
+git add -A
+git commit -m 'update'
+
+# update
+git push -f https://github.com/mejdi14/mejdi.git
+
 
 @echo off
   echo.>"E:\martin\final\mejdi\dblank.txt"
